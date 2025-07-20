3f02aa7a49fa9809dfaa3346dd43c59d801565da
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-07-20 13:28:20 -0400

    add no cities message

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -3,11 +3,17 @@ import { useCities } from "../hooks/useCities";
 
 import CityItem from "./CityItem";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList() {
   const { cities, isLoading } = useCities();
 
   if (isLoading) return <Spinner />;
+
+  if (cities.length < 1)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
 
   return (
     <ul className={styles.cityList}>
