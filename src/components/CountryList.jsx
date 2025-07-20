59e5fe91c4a175bd762d973a97294e2c6d5b75d3
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-07-20 14:24:04 -0400

    adding comments

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,22 +1,29 @@
+// Hooks
 import { useCities } from "../hooks/useCities";
 
+// Styles
 import styles from "./CountryList.module.css";
 
+// Components
 import CountryItem from "./CountryItem";
 import Message from "./Message";
 
 function CountryList() {
+  // Destructure cities and loading state from custom hook
   const { cities, isLoading } = useCities();
 
+  // Extract unique countries from the cities list
   const countries = cities.reduce((arr, city) => {
+    // Check if the country already exists in the array
     if (arr.map((el) => el.country).includes(city.country)) return arr;
+    // If not, add it to the array with country and emoji
     else return [...arr, { country: city.country, emoji: city.emoji }];
   }, []);
 
-  console.log(countries);
-
+  // Show a loading spinner while data is being fetched
   if (isLoading) return <Spinner />;
 
+  // Show a message if there are no cities yet
   if (cities.length < 1)
     return (
       <Message message="Add your first city by clicking on a city on the map" />
