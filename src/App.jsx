95c002b2072f47c93c10b5a8dea1500e4baf9448
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-07-22 22:08:17 -0400

    Optimizing bundle size

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
+import { lazy } from "react";
+
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 // Pages
-import Homepage from "./pages/Homepage";
-import Pricing from "./pages/Pricing";
-import Product from "./pages/Product";
-import Login from "./pages/Login";
-import AppLayout from "./pages/AppLayout";
+// import Homepage from "./pages/Homepage";
+// import Pricing from "./pages/Pricing";
+// import Product from "./pages/Product";
+// import Login from "./pages/Login";
+// import AppLayout from "./pages/AppLayout";
 
-// Fallback Page
-import PageNotFound from "./pages/PageNotFound";
+// import PageNotFound from "./pages/PageNotFound";
+
+// Pages
 
 // Components used inside protected routes
 import City from "./components/City";
@@ -19,6 +22,18 @@ import Form from "./components/Form";
 
 // Context Provider for cities data
 import { CitiesProvider } from "./contexts/CitiesContext";
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
+
+// Fallback Page
+const PageNotFound = lazy(() => import("./pages/Homepage"));
+
+// dist/assets/index-D2APp9la.css   31.03 kB │ gzip:   5.21 kB
+// dist/assets/index-DV965ESM.js   550.56 kB │ gzip: 162.07 kB
 
 function App() {
   return (
